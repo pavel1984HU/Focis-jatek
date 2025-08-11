9ae1b950cb9a5039788a33b539c42ced646abd12
pavel1984HU <adrianorosz84@gmail.com>
2025-08-11 05:23:08 +0200

    Create service-worker-v3.js

---
+++ service-worker-v3.js
@@ -0,0 +1,35 @@
+// service-worker-v3.js
+const CACHE_NAME = 'kickspark-v5';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    fetch(req).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(req, clone));
+      return res;
+    }).catch(() => caches.match(req).then(cached => cached || caches.match('./')))
+  );
+});
