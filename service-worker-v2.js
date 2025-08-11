ba261e4a1d3ddda035f8cdd6866ed2b7cf1e6894
pavel1984HU <adrianorosz84@gmail.com>
2025-08-11 05:09:55 +0200

    Update service-worker-v2.js

--- service-worker-v2.js
+++ service-worker-v2.js
@@ -12,12 +12,14 @@ self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null))))
   );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', (e) => {
   const req = e.request;
   if (req.method !== 'GET') return;
